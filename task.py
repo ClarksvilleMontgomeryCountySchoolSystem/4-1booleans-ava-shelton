01d8eddfd005cfc6b37cd1ef7c0e46349d694337
ava-shelton <ava.shelton065@students.cmcss.net>
2025-12-05 13:55:13 -0600

    Update task.py

--- task.py
+++ task.py
@@ -1,1 +1,51 @@
 # Paste code below then Commit changes [green button]
+#--------Group 1----------
+# Question: "Are you wearing shoes right now?"
+
+is_wearing_shoes = False   # You are not wearing shoes.
+
+# Question: "Did you eat breakfast today?"
+ate_breakfast_1 = False    # You ate breakfast this morning.
+ate_breakfast_2 = True   # You did not eat breakfast this morning.
+
+# Question: "Are you feeling awake?"
+is_awake = False     # You are feeling awake
+
+
+# Question: "Do you have a phone with you?"
+has_phone = True     # You have a phone with you.
+
+
+# Question: "Are you sitting near the sink?"
+
+near_sink = True    # You are not sitting near the sink.
+
+# ------Group 2--------
+
+# Question: "Are you NOT wearing a hat?"
+not_wearing_hat = False     # You have a hat on your head.
+
+
+# Question: "Did you NOT forget your homework?"
+not_forgot_homework = False    # You forgot your homework.
+
+
+# Question: "Are you NOT tired?"
+not_tired = False   # You don't need a nap.
+
+
+# Question: "Do you NOT have a pencil?"
+not_have_pencil = True     # You have a pencil
+
+
+# Question: "Are you NOT sitting by the teacher's desk?"
+not_in_front = False     # " Yes and the fridge too."
+
+
+
+# REFLECTION: Discuss with your partner
+Mostly easy just true and false questions some were more difficult than others
+# Which variable names were easiest to work with? Why?
+The regular ones
+# Which were most confusing? Why?
+The Not ones because it made me think more
